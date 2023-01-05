3989f937644bb7c6c115b7249e16ef7a57f264cd
JoelArias <joelariasparra3@gmail.com>
2023-01-05 20:36:04 -0300

    Api coingecko, mostrar 10 coins con mayor cap ok

--- cointracker/views.py
+++ cointracker/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import requests
+
+
 
 # Create your views here.
 
 def welcome(request):
-    return render(request, "cointracker/welcome.html")
+    data = getApi()
+    coins = get_coins()
+    return render(request, "cointracker/welcome.html", {"data": data, "coins": coins})
+
+def getApi():
+    url = "https://api.coingecko.com/api/v3/ping"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return f"Error {response.status_code}: {response.text}"
+    
+def get_coins():
+    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return f"Error {response.status_code}: {response.text}"
