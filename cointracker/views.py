970638c0791e24cc226d4ca59be50f17873ad7a2
JoelArias <joelariasparra3@gmail.com>
2023-01-06 19:07:50 -0300

    Vistas de trending y trust score listas

--- cointracker/views.py
+++ cointracker/views.py
@@ -1,31 +1,48 @@
 from django.shortcuts import render
+from pycoingecko import CoinGeckoAPI
 import requests
 
 
-
 # Create your views here.
+cg = CoinGeckoAPI()
 
 def welcome(request):
-    data = getApi()
-    coins = get_coins()
-    return render(request, "cointracker/welcome.html", {"data": data, "coins": coins})
+    topcoins = get_top_coins()
+    trending = get_trending_coins()
+    exchgvolume = get_exchanges()
+    return render(request, "cointracker/welcome.html", {"coins":topcoins, "trending":trending, "exchange":exchgvolume})
 
-def getApi():
-    url = "https://api.coingecko.com/api/v3/ping"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        return f"Error {response.status_code}: {response.text}"
-    
-def get_coins():
-    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
-    response = requests.get(url)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        return f"Error {response.status_code}: {response.text}"
+def get_top_coins():
+    cg = CoinGeckoAPI()
+    coins = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc', per_page=10, page=1, sparkline=False)
+    for coin in coins:
+        coin['current_price'] = '${:,.2f}'.format(coin['current_price'])
+        coin['market_cap'] = '${:,.2f}'.format(coin['market_cap'])
+    return coins
+
+
+
+#Cards de HTML
+def get_trending_coins():
+    cg = CoinGeckoAPI()
+    trending_coins = cg.get_search_trending()['coins'][:10]
+    return trending_coins
+
+
+def get_exchanges():
+  # Hacer la llamada a la API
+  response = requests.get("https://api.coingecko.com/api/v3/exchanges?per_page=10&page=1")
+
+  if response.status_code == 200:
+    data = response.json()
+    exchanges = []
+
+    # Iterar a través de cada exchange en la lista de exchanges
+    for exchange in data:
+      # Agregar el exchange a la lista
+      exchanges.append(exchange)
+
+    return exchanges
+  else:
+    return []
